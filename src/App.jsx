4c7bb369b7a3964aea9807c3151988bba42a86fb
mathaillah <mathaillah96@gmail.com>
2025-10-06 23:28:32 +0700

    react tictactoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
+import { useState } from "react";
 
-import { useState } from 'react'
-import Header from './components/Header'
+import Header from "./components/Header";
 
-
-
-function App() {
-  const[likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-  return(
-    <div>
-      <Header author="John Doe" />
-      <p>Likes: {likes}</p>
-      <button onClick={handleClick}>Like</button>
-    </div>
-  )   
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+function handleClick(i) {
+    if (squares[i]) return;
+
+    const nextSquares = squares.slice();
+
+    // nextSquares[i] = "X"; // ["X", null, null, null, null, null, null, null, null])
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+  <>
+    <div className="board">
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    </div>
+  </>
+);
+  }
+
